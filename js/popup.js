a9719f222c053be0c3b5703373fc564973d43fa1
maximus77777 <limax777777@gmail.com>
2018-03-25 02:21:22 +1000

    111

--- js/popup.js
+++ js/popup.js
@@ -1,21 +1,51 @@
+'use strict'
+
 var contact = document.querySelector(".btn-contacts");
 var popup = document.querySelector(".modal-login");
 var close = document.querySelector(".modal-close");
 var login = popup.querySelector("[name=login]");
-var password = popup.querySelector("[name=password]");
-var form = popup.querySelector("form");
+var email = popup.querySelector("[name=email]");
+var form = popup.querySelector(".form-login");
 
-form.addEventListener("submit", function (evt) {
-	evt.preventDefault();
-});
+var storage = localStorage.getItem("login");
+
 
 contact.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.add("modal-show");
-	login.focus();
+	
+
+	if (storage) {
+		login.value = storage;
+		email.focus();
+	  } else {
+		login.focus();
+	  }
 });
 
 close.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	popup.classList.remove("modal-show");
+	popup.classList.remove("modal-error");
 });
+
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+    if (!login.value || !email.value) {
+      
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+    } else {
+      localStorage.setItem("login", login.value);
+    }
+  });
+
+window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (popup.classList.contains("modal-show")) {
+        popup.classList.remove("modal-show");
+        popup.classList.remove("modal-error");
+      }
+    }
+  });
